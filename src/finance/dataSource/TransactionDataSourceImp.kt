63d8370302a8b92473a6765946b545b83dd4a217
malak.raef20@gmail.com <malak.raef20@gmail.com>
2025-04-09 18:45:03 +0200

    refactor data source implementation

--- src/finance/dataSource/TransactionDataSourceImp.kt
+++ src/finance/dataSource/TransactionDataSourceImp.kt
@@ -3,10 +3,11 @@ package finance.dataSource
 import finance.model.BalanceReport
 import finance.model.MonthSummary
 import finance.model.Transaction
+import finance.model.TransactionType
 
 
 class TransactionDataSourceImp : TransactionDataSource {
-    private val transactions : MutableMap<String,Transaction> = mutableMapOf()
+    private val transactions: MutableMap<String, Transaction> = mutableMapOf()
 
 
     override fun addTransaction(transaction: Transaction): Boolean {
@@ -16,45 +17,46 @@ class TransactionDataSourceImp : TransactionDataSource {
 
     override fun editTransaction(editedTransaction: Transaction): Boolean {
 
-        return if(checkIfTransactionExist(editedTransaction.id)){
+        return if (checkIfTransactionExist(editedTransaction.id)) {
             transactions[editedTransaction.id] = editedTransaction
             true
-        } else{
+        } else {
             false
         }
     }
 
     override fun deleteTransaction(id: String): Boolean {
-        return if(checkIfTransactionExist(id)){
+        return if (checkIfTransactionExist(id)) {
             transactions.remove(id)
             true
-        } else{
+        } else {
             false
         }
     }
 
-    override fun getAllTransactions(): List<Transaction>  = transactions.values.toList()
+    override fun getAllTransactions(): List<Transaction> = transactions.values.toList()
 
     override fun getTransactionById(id: String): Transaction? = transactions[id]
 
 
-
-    override fun getMonthlySummary(month : Int): MonthSummary {
-        val monthTransactions = transactions.filter { it.date.month == month }
+    override fun getMonthlySummary(month: Int): MonthSummary {
+        val monthTransactions = transactions.filter { it.value.date.monthValue == month }
         return MonthSummary(
-            month = month
+            month = monthTransactions
         )
     }
 
     // filter by category
     override fun getBalanceReport(): BalanceReport {
-        val income = transactions.filter { it.type ==TransactionsType.INCOME }.sumOf { it.quantity }
-        val expense = transactions.filter { it.type == TransactionsType.EXPENSE }.sumOf { it.quantity }
+        val listOfTransactions = getAllTransactions()
+        val income = listOfTransactions.filter { it.transactionType == TransactionType.INCOME }.sumOf { it.amount }
+
+        val expense = listOfTransactions.filter { it.transactionType == TransactionType.EXPENSE }.sumOf { it.amount }
         return BalanceReport(
             income, expense
         )
     }
 
-    private fun checkIfTransactionExist(id : String) =
+    private fun checkIfTransactionExist(id: String) =
         transactions.containsKey(id)
 }
