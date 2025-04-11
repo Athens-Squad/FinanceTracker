7ebff3748be9b41aed689efd44aa6b6688b6a51b
Bilal Azzam <blalazam065@gmail.com>
2025-04-11 07:30:37 +0200

    refactored transaction manager component to eliminate redundant function definitions without badly affecting the structure keeping SOC and Flexibility.

--- src/finance/logic/TransactionManager.kt
+++ src/finance/logic/TransactionManager.kt
@@ -1,21 +1,12 @@
 package finance.logic
 
+import finance.dataSource.TransactionDataSource
 import finance.model.MonthSummary
 import finance.model.Transaction
 import java.time.YearMonth
 
 // all functions
-interface TransactionManager {
-
-    fun addTransaction(transaction: Transaction)
-
-    fun deleteTransaction(id: String)
-
-    fun editTransaction(transaction: Transaction)
-
-    fun getAllTransactions(): List<Transaction>
-
-    fun getTransactionById(id: String): Transaction?
+interface TransactionManager: TransactionDataSource {
 
     //transaction list + balance + income +
     fun getSummaryMonth(month: YearMonth): MonthSummary
